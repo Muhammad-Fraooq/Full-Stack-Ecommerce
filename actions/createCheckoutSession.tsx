85ac843a14cdd9137d9c338ed286424b5d4cafa7
Muahmmad-Farooq <muhammad888xyz@gmail.com>
2025-01-25 12:17:22 +0500

    updated Files

--- actions/createCheckoutSession.tsx
+++ actions/createCheckoutSession.tsx
@@ -56,8 +56,8 @@ export async function CreateCheckoutSession(
       mode: "payment",
       allow_promotion_codes: true,
       payment_method_types: ["card"],
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?orderNumber=${metadata.orderNumber.slice(-10) ?? "N/A"}`,
-      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cart`,
+      success_url: `https://full-stack-ecommerce-website-seven.vercel.app/success?orderNumber=${metadata.orderNumber.slice(-10) ?? "N/A"}`,
+      cancel_url: `https://full-stack-ecommerce-website-seven.vercel.app/cart`,
       line_items: items.map((item) => ({
         price_data: {
           currency: "USD",
@@ -96,82 +96,3 @@ export async function CreateCheckoutSession(
   }
 }
 
-
-// "use server";
-// import stripe from "@/lib/stripe";
-// import { urlFor } from "@/sanity/lib/image";
-// import { CartItem } from "@/store";
-// import Stripe from "stripe";
-
-// export interface Metadata {
-//   orderNumber: string;
-//   customerName: string;
-//   customerEmail: string;
-//   clerkUserId: string;
-  
-// }
-// export interface GroupedCartItems {
-//   product: CartItem["product"];
-//   quantity: number;
-// }
-// export async function CreateCheckoutSession(
-//   items: GroupedCartItems[],
-//   metadata: Metadata
-// ) {
-//   try {
-//     const itemsWithoutPrice = items.filter((item) => !item.product.price);
-//     if (itemsWithoutPrice.length > 0) {
-//       throw new Error("Some items do not have a price. ");
-//     }
-
-//     const customers = await stripe.customers.list({
-//       email: metadata.customerEmail,
-//       limit: 1,
-//     });
-
-//     const customerId = customers.data.length > 0 ? customers.data[0].id : "";
-
-//     const sessionPayload: Stripe.Checkout.SessionCreateParams = {
-//       metadata: {
-//         orderNumber: metadata.orderNumber,
-//         customerName: metadata.customerName,
-//         customerEmail: metadata.customerEmail,
-//         clerkUserId: metadata.clerkUserId,
-//       },
-//       mode: "payment",
-//       allow_promotion_codes: true,
-//       payment_method_types: ["card"],
-//        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?orderNumber=${metadata.orderNumber.slice(-10) ?? "N/A" }`,
-//       // success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?orderNumber={CHECKOUT_SESSION_ID}`,
-//       // success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-//       cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cart`,
-//       line_items: items.map((item) => ({
-//         price_data: {
-//           currency: "USD",
-//           unit_amount: Math.round(item.product.price! * 100),
-//           product_data: {
-//             name: item.product.name || "Unnamed Product",
-//             description: item.product.description,
-//             metadata: {
-//               id: item.product._id,
-//             },
-//             images: item.product.image
-//               ? [urlFor(item.product.image).url()]
-//               : undefined,
-//           },
-//         },
-//         quantity: item.quantity,
-//       })),
-//     };
-//     if (customerId) {
-//       sessionPayload.customer = customerId;
-//     } else {
-//       sessionPayload.customer_email = metadata.customerEmail;
-//     }
-//     const session = await stripe.checkout.sessions.create(sessionPayload);
-//     return session.url;
-//   } catch (error) {
-//     console.error("Error creating checkout session:", error);
-//     throw error;
-//   }
-// }
